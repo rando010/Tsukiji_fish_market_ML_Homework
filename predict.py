4ad5be932d8d557067232615f5bcb62c1a45ea74
rando010 <56077487+rando010@users.noreply.github.com>
2020-05-09 20:30:45 -0500

    Add files via upload
    final commit

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ from joblib import load
 from preprocess import prep_data
 import pandas as pd
 
+
 def predict_from_csv(path_to_csv):
 
     df = pd.read_csv(path_to_csv)
@@ -14,16 +15,17 @@ def predict_from_csv(path_to_csv):
 
     return predictions
 
+
 if __name__ == "__main__":
     predictions = predict_from_csv("fish_holdout_demo.csv")
-    # print(predictions)
-######
+    print(predictions)
+    ######
 
-### WE WRITE THIS ###
+    ### WE WRITE THIS ###
     from sklearn.metrics import mean_squared_error
-    ho_predictions = predict_from_csv("fish_holdout.csv")
-    ho_truth = pd.read_csv("fish_holdout.csv")["Weight"].values
+
+    ho_predictions = predict_from_csv("fish_holdout_demo.csv")
+    ho_truth = pd.read_csv("fish_holdout_demo.csv")["Weight"].values
     ho_mse = mean_squared_error(ho_truth, ho_predictions)
     print(ho_mse)
 ######
-
--- train.py
+++ train.py
@@ -1,13 +1,45 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+
+import numpy as np
+
+from sklearn.tree import DecisionTreeRegressor
+
 from joblib import dump
+
 from preprocess import prep_data
 
-df = pd.read_csv("fish_participant.csv")
+from sklearn.model_selection import cross_validate
 
-X, y = prep_data(df)
+from sklearn.model_selection import KFold
 
-lr = LinearRegression()
-lr.fit(X, y)
+from sklearn.metrics import mean_squared_error
 
-dump(lr, "reg.joblib")
+fish_data = pd.read_csv("fish_participant.csv")
+
+print(fish_data.head)
+
+print(fish_data.dtypes)
+
+X, y = prep_data(fish_data)
+
+decisiontree = DecisionTreeRegressor()
+
+cross_validate(
+    decisiontree,
+    X,
+    y,
+    scoring="neg_mean_squared_error",
+    cv=KFold(random_state=123, shuffle=True),
+)["test_score"].mean()
+
+decisiontree.fit(X, y)
+
+fish_data_holdout = pd.read_csv("fish_holdout_demo.csv")
+
+X_hold, y_true = prep_data(fish_data_holdout)
+
+y_predict = decisiontree.predict(X_hold)
+
+print([y_true, y_predict])
+
+print(mean_squared_error(y_true, y_predict))
